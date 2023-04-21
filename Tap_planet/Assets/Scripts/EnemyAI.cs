27d2471ee60e9bc076e9116f43b31cff32d40a6f
lawi22 <80677662+lawi22@users.noreply.github.com>
2023-04-21 14:06:40 +0200

    Scuffed Raid but semi works

--- Tap_planet/Assets/Scripts/EnemyAI.cs
+++ Tap_planet/Assets/Scripts/EnemyAI.cs
@@ -15,80 +15,107 @@ public class EnemyAI : MonoBehaviour
 
     public Button startButton;
 
+    private float waitTime = 2f;
+    private float timer = 0.0f;
+
+    static int initialLength;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
 
+     initialLength = enemyList.Length;
+
     }
 
-    // Update is called once per frame
-    void Update()
+// Update is called once per frame
+void Update()
     {
-        bool isNullOrEmpty = !enemyList.Any();
 
-        if (isNullOrEmpty)
+        timer += Time.deltaTime;
+
+
+        if(timer > waitTime)
+        {
+            beginEnemyAI();
+            timer = timer - (waitTime + 0.7f);
+        }
+
+        bool isEmpty = !enemyList.Any();
+        if (isEmpty)
         {
             isListEmpty = true;
         }
-        else
-        {
-            beginEnemyAI();
-        }
+
     }
 
 
     public void beginEnemyAI()
     {
-
         while (!isListEmpty)
         {
-            int i = Random.Range(0, enemyList.Length);
+            
+                int i = Random.Range(0, initialLength);
 
 
-            if (enemyList[i] != null)
-            {
-                enemyList[i].GetComponent<RaidEnemyMovement>().timeToMove = true;
-                enemyList[i] = null;
-            }
-            else
-            {
-              
-                return;
-                //GameObject[] newEnemyList = new GameObject[enemyList.Length - 1];
+                if (enemyList[i] != null)
+                {
+                    enemyList[i].GetComponent<RaidEnemyMovement>().timeToMove = true;
+                    enemyList[i] = null;
+                }
+                else
+                {
+                    return;
 
-                //for (int j = 0; j < newEnemyList.Length; j++)
-                //{
-                //    if (enemyList[j] != null)
-                //    {
-                //        newEnemyList[i] = enemyList[j];
-                //    }
-                //}
+                    //GameObject[] newEnemyList = new GameObject[enemyList.Length - 1];
 
-                //enemyList = newEnemyList;
+                    //for (int j = 0; j < newEnemyList.Length; j++)
+                    //{
+                    //    if (enemyList[j] != null)
+                    //    {
+                    //        newEnemyList[i] = enemyList[j];
+                    //    }
+                    //}
 
-            }
+                    //enemyList = newEnemyList;
+                    //initialLength = newEnemyList.Length;
 
+                }
 
+    
 
 
         }
     }
 
+
+    //int i = Random.Range(0, initialLength);
+    //enemyList[i].GetComponent<RaidEnemyMovement>().timeToMove = true;
+
+    //enemyList[i] = enemyList[initialLength];
+    //enemyList[initialLength] = null;
+    //initialLength--;
+
+
+
+
 }
-        //int number = Random.Range(0, enemyList.Length);
+//int number = Random.Range(0, enemyList.Length);
 
-        //this.enemyList[number].GetComponent<RaidEnemyMovement>().timeToMove = true;
-
-
-
-            //for (int i = 0; i<enemyList.Length; i++)
-        
-            //int random = Random.Range(0, enemyList.Length);
+//this.enemyList[number].GetComponent<RaidEnemyMovement>().timeToMove = true;
 
 
-            //if (enemyList[random] != null && !enemyList[random].GetComponent<RaidEnemyMovement>().timeToMove) 
-            
-              //  enemyList[random].GetComponent<RaidEnemyMovement>().timeToMove = true;
-                //Debug.Log(enemyList[random]);
-            
-     
+
+//for (int i = 0; i<enemyList.Length; i++)
+
+//int random = Random.Range(0, enemyList.Length);
+
+
+//if (enemyList[random] != null && !enemyList[random].GetComponent<RaidEnemyMovement>().timeToMove) 
+
+//  enemyList[random].GetComponent<RaidEnemyMovement>().timeToMove = true;
+//Debug.Log(enemyList[random]);
+
+
