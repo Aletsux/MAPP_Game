8a67249ff4942d7f9bcb366a9e897d16ce6bdcbe
keowkitty <kioleesvensson@gmail.com>
2023-05-11 10:55:28 +0200

    .

---
+++ Tap_planet/Assets/Scripts/Raid/PlanetHit.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlanetHit : MonoBehaviour
+{
+    private bool startBlinking;
+    public float alpha;
+    protected Image image;
+    protected int blinkAmount = 8;
+    public float timeBetweenBlinks;
+    private float timer;
+    private bool isVisible;
+    private int blinkCount;
+
+    public GameObject hitSprite;
+
+    private List<GameObject> hitSprites = new();
+
+    protected virtual void Start()
+    {
+        GameObject[] activeSprites = GameObject.FindGameObjectsWithTag("PlanetSprite");
+
+        foreach (GameObject g in activeSprites)
+        {
+            GameObject newHitSprite = Instantiate(hitSprite, g.transform.position, g.transform.rotation, g.transform);
+            hitSprites.Add(newHitSprite);
+            newHitSprite.GetComponent<Image>().color = new Color(1, 0, 0, 0);
+        }
+    }
+
+    void Update()
+    {
+        if (startBlinking)
+        {
+            timer += Time.deltaTime;
+            if (timer >= timeBetweenBlinks)
+            {
+                timer = 0;
+                ActivateHitSprites();
+                isVisible = !isVisible;
+                blinkCount++;
+                if (blinkCount >= blinkAmount)
+                {
+                    startBlinking = false;
+                    blinkCount = 0;
+                    ResetAlpha();
+                }
+            }
+        }
+    }
+
+    public void StartBlinking(){
+        startBlinking = true;
+    }
+    private void ResetAlpha()
+    {
+        foreach (GameObject g in hitSprites)
+        {
+            if (isVisible)
+            {
+                alpha = 0.5f;
+            }
+            else
+            {
+                alpha = 0f;
+            }
+            g.GetComponent<Image>().color = new Color(1, 0, 0, 0);
+        }
+    }
+
+    private void ActivateHitSprites()
+    {
+        foreach (GameObject g in hitSprites)
+        {
+            if (isVisible)
+            {
+                alpha = 0.5f;
+            }
+            else
+            {
+                alpha = 0f;
+            }
+            g.GetComponent<Image>().color = new Color(1, 0, 0, alpha);
+        }
+    }
+}
