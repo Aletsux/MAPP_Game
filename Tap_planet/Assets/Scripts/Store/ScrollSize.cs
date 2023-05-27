10bb9a0ea6326649aec44b1e03a461e60b3b52e5
Aletsux <71812424+Aletsux@users.noreply.github.com>
2023-05-27 16:44:44 +0200

    Update ScrollSize.cs

--- Tap_planet/Assets/Scripts/Store/ScrollSize.cs
+++ Tap_planet/Assets/Scripts/Store/ScrollSize.cs
@@ -6,7 +6,7 @@ public class ScrollSize : MonoBehaviour
 {
     void Start()
     {
-        float h = FindObjectOfType<Canvas>().GetComponent<RectTransform>().rect.height - 350; // skapar varibel och lagrar canvasens höjd minus höjden på beskrivningspanelen
+        float h = FindObjectOfType<Canvas>().GetComponent<RectTransform>().rect.height - 500; // skapar varibel och lagrar canvasens höjd minus höjden på beskrivningspanelen
         RectTransform rt = gameObject.GetComponent<RectTransform>(); //skapar behållare för detta objekts rectTr
         rt.sizeDelta = new Vector2(1080, h); // sätter objektets rectTr
     }
